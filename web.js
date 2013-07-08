95eb024441595c0c48dfaf79301f836dddeb180d
Ubuntu <ubuntu@ip-172-31-19-96.us-west-2.compute.internal>
2013-07-08 04:11:48 +0000

    Fixes require in web.js

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
@@ -7,10 +8,13 @@ var index = fs.readFileSync('index.html', function(err,data){
     console.log(data);
 });
 
-var buffer = buf.toString(index);
+var buffer = new Buffer();
+buffer.write(index);
+
+var bufferString = buffer.toString(index);
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(bufferString);
 });
 
 var port = process.env.PORT || 5000;
