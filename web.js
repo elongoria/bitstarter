1d51eabcfc4b54509bed94a077e3ea85b5f7a5a5
Ubuntu <ubuntu@ip-172-31-19-96.us-west-2.compute.internal>
2013-07-08 03:13:09 +0000

    Adding changes to display index.html

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync('index.html', function(err,data){
+    if (err) throw err;
+    console.log(data);
+});
+
+var buffer = buf.toString(index);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
